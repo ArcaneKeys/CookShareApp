b1044c4dad495bd2b53bb46ebb487b968cceb48e
Artur Kąkol <artur.kakol2000@gmail.com>
2024-07-06 23:18:43 +0200

    Create basic components, #1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { AddRecipeComponent } from './add-recipe/add-recipe.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { FavoritesComponent } from './favorites/favorites.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    RecipesComponent,
+    AddRecipeComponent,
+    ShoppingListComponent,
+    CategoriesComponent,
+    FavoritesComponent
   ],
   imports: [
     BrowserModule,
