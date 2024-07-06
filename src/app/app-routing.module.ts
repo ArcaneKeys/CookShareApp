0c93328c4aabc0504695c7fd311e6c75c3a12735
Artur Kąkol <artur.kakol2000@gmail.com>
2024-07-06 23:29:17 +0200

    Create basic routes, #1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { AddRecipeComponent } from './add-recipe/add-recipe.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { FavoritesComponent } from './favorites/favorites.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'recipes', component: RecipesComponent },
+  { path: 'add-recipe', component: AddRecipeComponent },
+  { path: 'shopping-list', component: ShoppingListComponent },
+  { path: 'categories', component: CategoriesComponent },
+  { path: 'favorites', component: FavoritesComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
