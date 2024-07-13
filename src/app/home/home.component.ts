dd87452c804ba57e771e0f272cdc23d469e048a4
Artur Kąkol <artur.kakol2000@gmail.com>
2024-07-13 18:45:24 +0200

    Random recipes, #11

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { RecipeService } from '../recipe.service';
+import { Recipe } from '../models/recipe.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-  
+export class HomeComponent implements OnInit {
+  randomRecipes: Recipe[] = [];
+
+  constructor(private recipeService: RecipeService) {}
+
+  ngOnInit() {
+    this.recipeService.getRandomRecipes(3).subscribe({
+      next: recipes => this.randomRecipes = recipes,
+      error: err => console.error(err)
+    });
+  }
 }
